3ef481fa1e4dcefc19fd49ffca7f36beb65bde45
Myles Enriquez <enri.myles@gmail.com>
2018-04-09 19:31:35 -0700

    Add duration types.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
   render() {
-    return <div>Countdown Champ</div>;
+    return (
+      <div className="app">
+        <div className="app-title">Countdown to December 25, 2018</div>
+        <div>
+          <div className="clock clock-days">4 days</div>
+          <div className="clock clock-hours">22 hours</div>
+          <div className="clock clock-minutes">15 minutes</div>
+          <div className="clock clock-seconds">10 seconds</div>
+        </div>
+        <div>
+          <input placeholder='new date' />
+          <button>Submit</button>
+        </div>
+      </div>
+    );
   }
 }
 
